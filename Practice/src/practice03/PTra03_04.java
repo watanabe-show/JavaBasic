707aa4978e418aa490e05879a0feed54ec50f5f1
watanabe-show <thorgason440@gmail.com>
2018-04-26 18:52:08 +0900

    PTra03_05まで完了

--- Practice/src/practice03/PTra03_04.java
+++ Practice/src/practice03/PTra03_04.java
@@ -33,6 +33,19 @@ public class PTra03_04 {
 		 * ※ プログラムは何行書いても良いです
 		 */
 
-
+		if (num % 4 == 0 ) {
+			if (num % 100 ==0) {
+				if (num % 400 ==0) {
+					System.out.println(line + "年はうるう年です。");
+				}else {
+					System.out.println(line + "年はうるう年ではありません。");
+				}
+			}else {
+				System.out.println(line + "年はうるう年です。");
+			}
+		}else {
+			System.out.println(line + "年はうるう年ではありません。");
+		}
 	}
-}
+	}
+
--- Practice/src/practice03/PTra03_05.java
+++ Practice/src/practice03/PTra03_05.java
@@ -34,5 +34,20 @@ public class PTra03_05 {
 		 * ※ プログラムは何行書いても良いです
 		 */
 
+		if (num > 100 || num < 0) {
+			System.out.println("不正な値が入力されました");
+		}else {
+			if (num >= 75) {
+				System.out.println("雨が降る確率が高いです");
+			}else {
+				if (num < 75 && num >= 30) {
+					System.out.println("雨が振るかもしれないです");
+				}else {
+					if (num < 30) {
+						System.out.println("雨はまず降らないでしょう");
+					}
+				}
+			}
+		}
 	}
 }
